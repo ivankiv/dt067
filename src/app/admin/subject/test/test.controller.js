0cdf5b4b433e04882640bb6be27bdb9151a79208
ivankiv <ivan.ivankiv27@gmail.com>
2016-08-11 14:30:47 +0300

    Added posibility to edit any of Quizes by modal window

--- src/app/admin/subject/test/test.controller.js
+++ src/app/admin/subject/test/test.controller.js
@@ -12,34 +12,19 @@
             self.list = {};
             self.currenSubjectName = '';
             self.showMessageNoEntity = false;
-            self.wasNotEditTestMessage = false;
-            self.test = {};
-            self.test.subject_id = $stateParams.currentSubjectId;
-            self.subjectList = {};
 
             //methods
             self.getTestById = getTestById;
             self.deleteTest = deleteTest;
-            self.editTest = editTest;
-            self.updateTest = updateTest;
-            self.getSubjects = getSubjects;
             self.getOneSubject = getOneSubject;
             self.showAddTestForm = showAddTestForm;
             self.showEditTestForm = showEditTestForm;
-            self.showAddForm = showAddForm;
-            self.HideFormTest = HideFormTest;
 
             activate();
 
             function activate() {
                 getOneSubject();
                 getTestById();
-            }
-
-            function getSubjects() {
-                subjectService.getSubjects().then(function(response) {
-                    self.subjectList = response.data;
-                    });
             }
 
             function getOneSubject() {
@@ -49,7 +34,7 @@
             }
             function getTestById() {
                 testService.getTestById($stateParams.currentSubjectId).then(function(response) {
-                    if(response.data.response == 'no records') {
+                    if(response.data.response === 'no records') {
                         self.showMessageNoEntity = true;
                     } else {
                         self.list = response.data;
@@ -63,18 +48,15 @@
                     plain: false
                 }).then(function() {
                     testService.deleteTest(testId).then(function(response) {
-                        if(response.data.response == 'ok') {
+                        if(response.data.response === 'ok') {
                             getTestById();
                         }
                     })
                 });
             }
 
-            function updateTest() {
-                testService.updateTest(self.test.test_id, self.test).then(updateTestComplete);
-            }
-
             function showAddTestForm() {
+                console.log( $uibModal);
                 var modalInstance = $uibModal.open({
                     templateUrl: 'app/admin/subject/test/add-test.html',
                     controller: 'TestModalController as tests',
@@ -103,49 +85,11 @@
                 });
                 modalInstance.result.then(function() {
                     ngDialog.open({template: '<div class="ngdialog-message"> \
-						  Тест успішно додано!</div>'
+						  Зміни збережено!</div>'
                     });
                     self.showMessageNoEntity = false;
                     getTestById();
                 })
-            }
-
-            function updateTestComplete(response) {
-                if(response.status == 400) {
-                    ngDialog.open({template: '<div class="ngdialog-message"> Назва тесту не унікальна!</div>'
-                    });
-                }
-
-                if(response.data.response == 'error') {
-                    self.wasNotEditTestMessage = true;
-                }
-
-                if(response.data.response == "ok") {
-                    self.test = {};
-                    self.showEditTestForm = false;
-                    getTestById();
-
-                }
-            }
-
-            function editTest(currentTest) {
-                getSubjects();
-                // self.showAddTestForm = false;
-                self.showEditTestForm = true;
-                self.test = currentTest;
-            }
-
-             function showAddForm() {
-                 self.showEditTestForm = false;
-                // self.showAddTestForm = true;
-                self.test.subject_id = $stateParams.currentSubjectId;
-            }
-
-            function HideFormTest() {
-                // self.showAddTestForm = false;
-                self.showEditTestForm = false;
-                self.wasNotEditTestMessage = false;
-                self.test = {};
             }
         }
 }());
--- src/app/admin/subject/test/test.modal.controller.js
+++ src/app/admin/subject/test/test.modal.controller.js
@@ -3,22 +3,59 @@
 
     angular.module('app')
         .controller('TestModalController', testModalController);
-        testModalController.$inject = ['testService', '$stateParams', 'appConstants', '$uibModalInstance', 'currentTest', 'ngDialog'];
+        testModalController.$inject = ['testService', '$stateParams', 'subjectService', '$uibModalInstance', 'currentTest', 'ngDialog'];
 
-        function testModalController(testService, $stateParams, appConstants, $uibModalInstance,  currentTest, ngDialog) {
+        function testModalController(testService, $stateParams, subjectService,  $uibModalInstance,  currentTest, ngDialog) {
             var self = this;
 
             //variables
             self.test = {};
+            self.test.test_id = currentTest.test_id;
             self.test.subject_id = $stateParams.currentSubjectId;
+            self.currentTest = currentTest;
+            self.subjectList = {};
+            self.wasNotEditTestMessage = false;
 
             //methods
             self.addTest = addTest;
+            self.updateTest = updateTest;
+            self.getSubjects = getSubjects;
             self.cancelForm = cancelForm;
 
+            activate();
+
+            function activate() {
+                getSubjects();
+            }
 
             function addTest() {
                 testService.addTest(self.test).then(addTestComplete)
+            }
+
+            function updateTest() {
+                testService.updateTest(self.test.test_id, self.currentTest).then(updateTestComplete);
+            }
+
+            function getSubjects() {
+                subjectService.getSubjects().then(function(response) {
+                    self.subjectList = response.data;
+                });
+            }
+
+            function updateTestComplete(response) {
+                if(response.status === 400) {
+                    ngDialog.open({template: '<div class="ngdialog-message"> На сервері вже є предмет з такою назвою!</div>'
+                    });
+                }
+
+                if(response.data.response === 'error') {
+                    self.wasNotEditTestMessage = true;
+                }
+
+                if(response.data.response === "ok") {
+                    self.test = {};
+                    $uibModalInstance.close();
+                }
             }
 
             function cancelForm () {
@@ -26,9 +63,9 @@
             }
 
             function addTestComplete(response) {
-                if(response.data.response == "ok") {
+                if(response.data.response === "ok") {
                     self.test = {};
-                    $uibModalInstance.close(response);
+                    $uibModalInstance.close();
                 }
             }
         }
