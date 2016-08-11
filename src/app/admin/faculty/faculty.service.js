ba84219d7ea1c289489a135993d16e68945bb587
Dmutro Usenko <desent666@gmail.com>
2016-08-11 22:09:58 +0300

    Add

--- src/app/admin/faculty/faculty.service.js
+++ src/app/admin/faculty/faculty.service.js
@@ -2,24 +2,61 @@
     'use strict';
 
     angular.module('app')
-        .factory('facultyFactory', facultyFctr);
-    facultyFctr.$inject = ['$http', 'appConstants', '$state'];
+        .factory('facultyService', facultyService);
+        facultyService.$inject = ['$http', 'appConstants'];
 
-    function facultyFctr ($http, appConstants) {
+    function facultyService ($http, appConstants) {
         return {
             getFaculties: getFaculties,
+            getOneFaculty: getOneFaculty,
             countFaculties: countFaculties,
-            getRecordsRange: getRecordsRange
+            getRecordsRange: getRecordsRange,
+            addFaculty: addFaculty,
+            deleteFaculty: deleteFaculty,
+            editFaculty: editFaculty
         };
 
         function getFaculties() {
             return $http.get(appConstants.getFaculties)
+                .then(fulfilled, rejected);
         }
+
+        function getOneFaculty(currentId) {
+            return $http.get(appConstants.getOneFaculty + currentId)
+                .then(fulfilled, rejected);
+            }
+
         function countFaculties() {
             return $http.get(appConstants.countFaculties)
+                .then(fulfilled, rejected);
         }
-        function getRecordsRange() {
-            return $http.get(appConstants.getRangeOfFacultiesURL)
+
+        function getRecordsRange(quantity, begin) {
+            return $http.get(appConstants.getRangeOfFaculties + "/" + quantity +"/" + begin)
+                .then(fulfilled, rejected);
+        }
+
+        function addFaculty(data) {
+            return $http.post(appConstants.addFaculty, data)
+                .then(fulfilled, rejected);
+        }
+
+        function deleteFaculty(faculty_id) {
+            return $http.delete(appConstants.delFaculty + faculty_id)
+                .then(fulfilled, rejected);
+        }
+
+        function editFaculty(faculty_id, data) {
+            return $http.post(appConstants.editFaculty + faculty_id, data)
+                .then(fulfilled, rejected);
+        }
+
+        function fulfilled(response) {
+            return response;
+        }
+
+        function rejected(response) {
+            return response;
         }
     }
-})();
+}());
