0a1efd01b4a6d4f553d4802f082e91b4158ab86e
andriyt85 <andriyt85@ukr.net>
2016-08-12 11:06:56 +0300

    Added group functionality into design pages

--- src/app/admin/group/group.service.js
+++ src/app/admin/group/group.service.js
@@ -3,23 +3,53 @@
 
     angular.module('app')
         .factory('groupService', groupService);
-        groupService.$inject = ['$http', 'appConstants'];
+    groupService.$inject = ['$http', 'appConstants'];
 
-        function groupService ($http, appConstants) {
-            return {
-                getGroups: getGroups
-            };
+    function groupService ($http, appConstants) {
+        return {
+            getGroups: getGroups,
+            getOneGroup: getOneGroup,
+            countGroups: countGroups,
+            getRecordsRange: getRecordsRange,
+            addGroup: addGroup,
+            deleteGroup: deleteGroup,
+            editGroup: editGroup
+        };
 
-            function getGroups() {
-                return $http.get(appConstants.getGroups)
-                    .then(fulfilled, rejected);
-            }
-
-            function fulfilled(response) {
-                return response;
-            }
-            function rejected(response) {
-                return response;
-            }
+        function getGroups() {
+            return $http.get(appConstants.getGroups)
+                .then(fulfilled, rejected);
         }
+        function getOneGroup(currentId) {
+            return $http.get(appConstants.getOneGroup + currentId)
+                .then(fulfilled, rejected);
+        }
+        function countGroups() {
+            return $http.get(appConstants.countGroups)
+                .then(fulfilled, rejected);
+        }
+        function getRecordsRange(quantity, begin) {
+            return $http.get(appConstants.getRangeOfGroups + "/" + quantity +"/" + begin)
+                .then(fulfilled, rejected);
+        }
+        function addGroup(data) {
+            return $http.post(appConstants.addGroup, data)
+                .then(fulfilled, rejected);
+        }
+        function deleteGroup(group_id) {
+            return $http.delete(appConstants.delGroup + group_id)
+                .then(fulfilled, rejected);
+        }
+        function editGroup(group_id, data) {
+            return $http.post(appConstants.editGroup + group_id, data)
+                .then(fulfilled, rejected);
+        }
+
+        function fulfilled(response) {
+            return response;
+        }
+        function rejected(response) {
+            return response;
+        }
+    }
 }());
---
+++ src/app/admin/group/groupModal.controller.js
@@ -0,0 +1,91 @@
+(function () {
+    'use strict';
+    angular.module('app')
+        .controller('groupModalController', groupModalController);
+
+    groupModalController.$ingect = ['grouopService','facultyService', 'specialityService', 'appConstants', '$uibModalInstance', 'currentGroup', 'ngDialog'];
+
+    function groupModalController(groupService, appConstants, facultyService, specialityService, $uibModalInstance,  currentGroup, ngDialog) {
+        var self = this;
+        self.currentGroup = currentGroup;
+        self.duplicateGroupsMessage = false;
+        self.incorrectEnteredDataMessage = false;
+        self.specialityList = {};
+        self.facultyList = {};
+
+        self.group = currentGroup;
+        self.addGroup = addGroup;
+        self.cancelForm = cancelForm;
+        self.updateGroup = updateGroup;
+        self.getSpeciality = getSpeciality;
+        self.getFaculty = getFaculty;
+
+        activate();
+
+        function activate() {
+            getSpeciality();
+            getFaculty();
+        }
+
+        function getSpeciality() {
+            specialityService.getSpecialities().then( function(response) {
+                self.specialityList = response.data;
+            });
+        }
+
+        function getFaculty() {
+            facultyService.getFaculties().then( function(response) {
+                self.facultyList = response.data;
+            });
+        }
+
+        function addGroup() {
+            groupService.addGroup(self.group).then(addFinish, addError)
+        }
+
+        function cancelForm() {
+            $uibModalInstance.dismiss('cancel');
+        }
+
+        function addFinish(response) {
+            if(response.status == 400) {
+                self.duplicateGroupsMessage = true;
+                return;
+            }
+
+            if(response.data.response == "ok") {
+                self.group = {};
+                $uibModalInstance.close(response);
+            }
+
+        }
+        function addError(response) {
+            alert("Oops!");
+            console.log(response)
+        }
+
+        function updateGroup() {
+            groupService.editGroup(appConstants.currentID, self.group)
+                .then(updateComplete, addError);
+        }
+
+        function updateComplete(response) {
+            if(response.status == 400) {
+                self.duplicateGroupsMessage = true;
+                return;
+            }
+
+            if(response.data.response == 'ok') {
+                self.currentGroup = {};
+                $uibModalInstance.close();
+                ngDialog.open({template: '<div class="ngdialog-message"> \
+						  Зміни збережено!</div>',
+                    plain: 'true'
+                });
+            }
+        }
+
+    }
+
+
+}());
