47ff2a920eb5d14dc29238188c8ec4d48b3e7fc9
ramon4uk <mygalroman@gmail.com>
2016-08-29 22:41:37 +0300

    test-player controller skeleton and small bug fixing

--- src/app/user/tests/test-player/test-player.controller.js
+++ src/app/user/tests/test-player/test-player.controller.js
@@ -1,1 +1,26 @@
+(function(){
+    'use strict';
+
+    angular.module('app')
+        .controller('TestsController', TestsController);
+    TestsController.$inject = ['testService', 'scheduleService', 'testPlayerService'];
+
+    function TestsController (testService, scheduleService, testPlayerService) {
+        var self = this;
+
+        //variables
+        self.listOfEvents = {};
+
+        //methods
+
+
+        activate();
+
+        function activate() {
+            checkAttempts(self.user_id, self.test_id);
+            getQuestions();
+            startTest();
+        }
+    }
+}());
 
