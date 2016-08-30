8bb2f477007e184d3a8d33a2da533dfedf0746db
Ivan Ivankiv <ivan.ivankiv27@gmail.com>
2016-08-30 20:32:03 +0300

    Added method to get list of Questions for current test to display on the page

--- src/app/user/tests/test-player/test-player.controller.js
+++ src/app/user/tests/test-player/test-player.controller.js
@@ -4,17 +4,15 @@
     angular.module('app')
         .controller('TestPlayerController', TestPlayerController);
 
-    TestPlayerController.$inject = ['$stateParams', 'questionsService', 'testService', 'scheduleService', 'testPlayerService', 'adminService', '$uibModal'];
+    TestPlayerController.$inject = ['testDetailsService', '$stateParams', 'questionsService', 'testService', 'scheduleService', 'testPlayerService', 'adminService', '$uibModal'];
 
-    function TestPlayerController ($stateParams, questionsService, testService, scheduleService, testPlayerService, adminService, $uibModal) {
+    function TestPlayerController (testDetailsService, $stateParams, questionsService, testService, scheduleService, testPlayerService, adminService, $uibModal) {
 
         var self = this;
 
         //variables
         self.user_id = 2;
         self.test_id = $stateParams.currentTestId;
-        self.levelOfQuestion = 1;
-        self.numberOfQuestions = 5;
         self.listOfQuestions = [];
 
         //methods
@@ -23,7 +21,7 @@
         activate();
 
         function activate() {
-            getQuestionsByLevelRand();
+            getTestDetailsByTest();
             checkAttempts(self.user_id,self.test_id);
         }
 
@@ -32,19 +30,29 @@
             console.log(checked);
                 if(checked){
                     ngDialog.open({
-                                template:'<div class="ngdialog-message">Перевищена кількість спроб здати тест!</div>',
+                                template:'<div class="ngdialog-message">РџРµСЂРµРІРёС‰РµРЅР° РєС–Р»СЊРєС–СЃС‚СЊ СЃРїСЂРѕР± Р·РґР°С‚Рё С‚РµСЃС‚!</div>',
                                 plain:true
                     })
                 }
         }
 
-        function getQuestionsByLevelRand() {
-            questionsService.getQuestionsByLevelRand(self.test_id, self.levelOfQuestion, self.numberOfQuestions)
-                .then(function(response) {
-                    console.log('from questionsController', response.data);
-                    self.listOfQuestions = response.data;
+        function getTestDetailsByTest() {
+            testDetailsService.getTestDetailsByTest(self.test_id).then(getTestDetailsByTestComplete)
+        }
+        function getTestDetailsByTestComplete(response) {
+                angular.forEach(response.data, function(testDetail) {
+                    getQuestionsByLevelRand(testDetail.level, testDetail.tasks);
                 });
-            console.log('from questionsController', self.listOfQuestions);
+            console.log(self.listOfQuestions);
+        }
+
+        function getQuestionsByLevelRand(levelOfQuestion, numberOfQuestions) {
+            questionsService.getQuestionsByLevelRand(self.test_id, levelOfQuestion, numberOfQuestions)
+                .then(function(response) {
+                    angular.forEach(response.data, function(question) {
+                        self.listOfQuestions.push(question);
+                    });
+                });
         }
 
     }
