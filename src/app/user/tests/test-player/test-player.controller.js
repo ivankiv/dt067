6bf7a3b7f1582d7cd8f61dcd89788277fc62f990
ramon4uk <mygalroman@gmail.com>
2016-09-05 15:06:52 +0300

    Test-player improvementss

--- src/app/user/tests/test-player/test-player.controller.js
+++ src/app/user/tests/test-player/test-player.controller.js
@@ -47,8 +47,11 @@
             return defer.promise;
         }
         function chooseQuestion(question_index) {
+            questionsId[self.currentQuestion_index].answer_ids = [];
             localStorage.setItem("currentQuestionsId", JSON.stringify(questionsId));
-            if(question_index === 19)
+            if(question_index === self.listOfQuestionsId.length -1){
+                $state.go('test', {questionIndex:0});
+            }
             $state.go('test', {questionIndex:question_index});
         }
 
