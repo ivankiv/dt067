4f390613aad292cc7eb5956d72509ab087b0fa02
Ivan Ivankiv <ivan.ivankiv27@gmail.com>
2016-09-09 07:42:23 +0300

    Fixed small bug in datepicker

--- src/app/admin/subject/schedules/schedule.modal.controller.js
+++ src/app/admin/subject/schedules/schedule.modal.controller.js
@@ -74,7 +74,7 @@
             function addScheduleComplete (response) {
                 if(response.data.response === 'ok') {
                     $uibModalInstance.close();
-                    self.schedule = {};
+                    self.schedule = {subject_id: null, event_date: new Date()};
                 }
                 if(response.status === 400) {
                     self.alreadyExistInSchedule = true;
@@ -84,7 +84,7 @@
             function updateScheduleComplete(response) {
                 if(response.data.response === 'ok') {
                     $uibModalInstance.close();
-                    self.currentSchedule = {};
+                    self.currentSchedule = {subject_id: null, event_date: new Date()};
                 }
             }
 
