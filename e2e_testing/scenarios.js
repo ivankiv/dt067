8f087012151fd8d48e0bdd399ac1c7dfdf67148a
ivankiv <ivan.ivankiv27@gmail.com>
2016-09-15 12:47:56 +0300

    Added some more end-to-end test

--- e2e_testing/scenarios.js
+++ e2e_testing/scenarios.js
@@ -2,36 +2,61 @@
 
 describe('D-Tester App E2E Testing', function() {
 
+    function toEnter() {
+        var login = element(by.model('login.user.username'));
+        var password = element(by.model('login.user.password'));
+        var loginButton = element(by.css('[value="Вхід"]'));
+
+        login.sendKeys('admin');
+        password.sendKeys('dtapi_admin');
+        loginButton.click();
+    }
+
     it('should automatically redirect to / when location hash/fragment is empty', function() {
 
         browser.get('~pupkin/dt067/dev/#/index.html');
+
+        toEnter();
+
         expect(browser.getLocationAbsUrl()).toMatch("/");
+
+        browser.get('~pupkin/dt067/dev/#/admin/subject');
 
     });
 
     describe('subjects', function() {
-        beforeEach(function() {
-            browser.get('~pupkin/dt067/dev/#/admin/subject');
-        });
+
 
         it('should have a title', function() {
-            expect(browser.getTitle()).
-                toEqual('D-Tester');
+            expect(browser.getTitle()).toEqual('D-Tester');
         });
 
         it('should show the number of subjects as', function() {
             expect(element.all(by.repeater('subject in subjects.list'))
-            .count()).toEqual(12);
+                .count()).toEqual(5);
         });
 
         it('should show the subject name as', function() {
-            element(by.model('subjects.showSearch')).sendKeys('aaa');
+            element(by.model('subjects.textSearch')).sendKeys('aaa');
             expect(element.all(by.repeater('subject in subjects.list'))
                 .count()).toEqual(1);
             var subject_name = element.all(by.repeater('subject in subjects.list'))
                 .first().element(by.binding('subject.subject_name'));
 
             expect(subject_name.getText()).toContain('aaa');
+
+        });
+
+        it('should show the subject name as', function() {
+            var search = element(by.model('subjects.textSearch'));
+            search.clear();
+            search.sendKeys('a');
+            expect(element.all(by.repeater('subject in subjects.list'))
+                .count()).toEqual(4);
+            var subject_name = element.all(by.repeater('subject in subjects.list'))
+                .first().element(by.binding('subject.subject_name'));
+
+            expect(subject_name.getText()).toContain('Основи програмування на JAVA');
 
         });
     });
