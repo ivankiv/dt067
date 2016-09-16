0fb32793d683a54eed31c4dff7ea46df41a115bd
ivankiv <ivan.ivankiv27@gmail.com>
2016-09-16 15:06:48 +0300

    Added some more e2e tests to subject.controller

--- e2e_testing/runner_scenarios/scenarios_subject.js
+++ e2e_testing/runner_scenarios/scenarios_subject.js
@@ -5,6 +5,9 @@ describe('D-Tester App E2E Testing', function() {
     var subjectNameForTestingFilter = "Основи JavaScript";
     var numberOfSubjectsPerPage = 5;
 
+    // Find the element with ng-model="subjects.textSearch"
+    var searchInput = element(by.model('subjects.textSearch'));
+
 
     function toEnter() {
         var login = element(by.model('login.user.username'));
@@ -17,7 +20,6 @@ describe('D-Tester App E2E Testing', function() {
     }
 
     it('should automatically redirect to / when location hash/fragment is empty', function() {
-
         browser.get('~pupkin/dt067/dev/#/index.html');
 
         toEnter();
@@ -25,7 +27,6 @@ describe('D-Tester App E2E Testing', function() {
         expect(browser.getLocationAbsUrl()).toMatch("/");
 
         browser.get('~pupkin/dt067/dev/#/admin/subject');
-
     });
 
     describe('Test for subjects', function() {
@@ -40,8 +41,6 @@ describe('D-Tester App E2E Testing', function() {
         });
 
         it('should filter results', function() {
-            // Find the element with ng-model="subjects.textSearch"
-            var searchInput = element(by.model('subjects.textSearch'));
 
             // Type "Основи JavaScript" in searchInput
             searchInput.sendKeys(subjectNameForTestingFilter);
@@ -56,6 +55,34 @@ describe('D-Tester App E2E Testing', function() {
             // Verify that name of subject after filtering is Основи JavaScript
             expect(subject_name.getText()).toContain('Основи JavaScript');
 
+        });
+    });
+
+    describe('Testing CRUD for subject', function() {
+        var buttonAddSubject = element(by.css('[title="Додати предмет"]'));
+        var subjectName = 'Oснови JAVA';
+
+        it('should add new subject to subject list', function() {
+
+            buttonAddSubject.click();
+
+            var enterSubjectName = element(by.model('subjects.subject.subject_name'));
+            var enterSubjectDescription = element(by.model('subjects.subject.subject_description'));
+            var submit = element.all(by.buttonText('Додати предмет'));
+
+            enterSubjectName.sendKeys(subjectName);
+            enterSubjectDescription.sendKeys(subjectName);
+
+            submit.click();
+
+            searchInput.clear();
+            searchInput.sendKeys(subjectName);
+
+            var subject_name = element.all(by.repeater('subject in subjects.list'))
+                .first().element(by.binding('subject.subject_name'));
+
+            // Verify that name of subject after filtering is Основи JavaScript
+            expect(subject_name.getText()).toContain(subjectName);
         });
     });
 });
