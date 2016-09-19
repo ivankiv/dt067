d78f615a4dd59cc6ca0ce45f26f9fa614b9c57e2
AndriiMelnyk <andrewmelnyk9292@gmail.com>
2016-09-20 00:46:46 +0300

    added functionality to show results like charts

--- src/app/admin/result/testResult.controler.js
+++ src/app/admin/result/testResult.controler.js
@@ -26,6 +26,36 @@
         self.subjectName = '';
         self.groupName = '';
 
+        self.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        self.series = ['Series A', 'Series B'];
+        self.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+        self.data = [
+            [65, 59, 80, 81, 56, 55, 40],
+            [28, 48, 40, 19, 86, 27, 90]
+        ];
+        self.options = {
+            scales: {
+                yAxes: [
+                    {
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }
+                ]
+            }
+        };
+
+        self.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+
         activate();
 
         function activate() {
